eb5f1ecd8758d8f4367d8153c2fe702cdd340bb2
mac <mac@appledeMacBook-Pro.local>
2026-06-12 06:25:19 +0800

    feat: wrap App with ConfigProvider

--- src/cscode/web/src/main.tsx
+++ src/cscode/web/src/main.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { ConfigProvider } from './context/ConfigContext';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ConfigProvider>
+      <App />
+    </ConfigProvider>
+  </React.StrictMode>
+);
